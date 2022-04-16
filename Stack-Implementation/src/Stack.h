8ac1db82fd33c30a92fd648cb6b612157b0aa564
alfonsodelr <delrosarioluis22@gmail.com>
2022-04-16 01:47:32 -0700

    Moved definitions to Stack header file

--- Stack-Implementation/src/Stack.h
+++ Stack-Implementation/src/Stack.h
@@ -1,6 +1,7 @@
 #ifndef STACK_
 #define STACK_
 
+#define DEFAULT_SIZE 6
 #include "StackInterface.h"
 #include "Stack.h"
 
@@ -8,14 +9,50 @@ template<class ItemType>
 class Stack : public StackInterface <ItemType>
 {
 public:
+    Stack();
     bool isEmpty() const;
     bool push(const ItemType& newEntry);
     bool pop();
     ItemType peek() const;
     ~Stack() {}
 private:
-    ItemType m_var = 0;
+    int size = DEFAULT_SIZE;
+    ItemType * m_stack = nullptr;
 };
 
+template<class ItemType>
+Stack<ItemType>::Stack() 
+{
+    m_stack = new ItemType(size);
+}
+
+template<class ItemType>
+bool Stack<ItemType>::isEmpty() const 
+{
+    std::cout << "isEmpty()";
+    return false;
+}
+
+template<class ItemType>
+bool Stack<ItemType>::push(const ItemType& newEntry) 
+{
+    std::cout << "push(): " << newEntry;
+    return false;
+}
+
+template<class ItemType>
+bool Stack<ItemType>::pop()
+{
+    std::cout << "pop()";
+    return false;
+}
+
+template<class ItemType>
+ItemType Stack<ItemType>::peek() const
+{
+    std::cout << "peek()";
+    return m_var;
+}
 
 #endif
+
--- Stack-Implementation/src/main.cpp
+++ Stack-Implementation/src/main.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 #include "Stack.h"
 
-class name {
-public:
-	name() {
-		std::cout << "called";
-	}
-	void print_nice() {
-		std::cout << "nice";
-	}
-};
 
 int main() {
 
-	Stack <int> my_stack;
-
-	my_stack.isEmpty();
+	
 	
 	return 0;
 
