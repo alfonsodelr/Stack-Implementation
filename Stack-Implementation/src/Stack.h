fa497dd80a41cebf9453f412617354edc9adc03d
alfonsodelr <delrosarioluis22@gmail.com>
2022-04-17 02:49:51 -0700

    Working Version of ADT Stack

--- Stack-Implementation/src/Stack.h
+++ Stack-Implementation/src/Stack.h
@@ -14,45 +14,106 @@ public:
     bool push(const ItemType& newEntry);
     bool pop();
     ItemType peek() const;
-    ~Stack() {}
+    void print() const;
+    ~Stack() override;
 private:
-    int size = DEFAULT_SIZE;
+    int m_size = 0;
+    int m_capacity = DEFAULT_SIZE;
     ItemType * m_stack = nullptr;
 };
+
+/////////////////////////////////////////////////////////////////////////
 
 template<class ItemType>
 Stack<ItemType>::Stack() 
 {
-    m_stack = new ItemType(size);
+    std::cout << "Stack()\n";
+    m_stack = new ItemType[m_capacity];
+    std::cout << std::endl;
+}
+
+template<class ItemType>
+Stack<ItemType>::~Stack() 
+{
+    delete[] m_stack;
 }
 
 template<class ItemType>
 bool Stack<ItemType>::isEmpty() const 
 {
-    std::cout << "isEmpty()";
-    return false;
+    std::cout << "isEmpty()\n\n";
+    if (m_size == 0) 
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 template<class ItemType>
 bool Stack<ItemType>::push(const ItemType& newEntry) 
 {
-    std::cout << "push(): " << newEntry;
-    return false;
+    if (m_size == m_capacity) 
+    {
+        std::cout << "max push(" << newEntry << "):\n";
+        m_capacity *= 2;
+        std::cout << "m_capacity: " << m_capacity << std::endl;
+        ItemType* to_delete = m_stack;
+        ItemType* temp_stack = new ItemType[m_capacity];
+        
+        for (int i = 0; i < m_size; i++)
+        {
+            temp_stack[i] = m_stack[i];
+        }
+
+        temp_stack[m_size] = newEntry;
+        m_stack = temp_stack;
+        delete [] to_delete;
+        m_size++;
+        return true;
+    }
+    else
+    {
+        std::cout << "normal push(" << newEntry << "):\n";
+        m_stack[m_size] = newEntry;
+        m_size++;
+        return true;
+    }
 }
 
 template<class ItemType>
 bool Stack<ItemType>::pop()
 {
-    std::cout << "pop()";
-    return false;
+    std::cout << "pop()\n\n";
+    if (m_size == 0) 
+    { 
+        return false; 
+    }
+
+    m_size--;
+    return true;
 }
 
 template<class ItemType>
 ItemType Stack<ItemType>::peek() const
 {
-    std::cout << "peek()";
-    return m_var;
+    std::cout << "peek()\n";
+    return m_stack[m_size - 1];
 }
+
+template<class ItemType>
+void Stack<ItemType>::print() const
+{
+    std::cout << "print()\n";
+    for (int i = 0; i < m_size; i++) 
+    {
+        std::cout << m_stack[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
+
 
 #endif
 
--- Stack-Implementation/src/main.cpp
+++ Stack-Implementation/src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "Stack.h"
+#include <string>
 
+int main() 
+{
 
-int main() {
+	Stack<std::string> _list;
 
-	
-	
+	_list.push("this");
+	_list.push("is");
+	_list.push("fun");
+	_list.print();
+	_list.pop();
+	std::cout << _list.peek() << std::endl << std::endl;
+	_list.print();
+
 	return 0;
 
 }
